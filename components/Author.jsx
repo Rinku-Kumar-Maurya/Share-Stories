14d65207cb25f1505fa2b6c3d3a989279c432472
Rinku Kumar Maurya <rinkukumarmaurya221@gmail.com>
2021-12-31 22:46:20 +0530

    implement Author component

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
-const Author = () => {
+import Image from 'next/image'
+
+const Author = ({ author }) => {
     return (
-        <div>
-            Author
+        <div className='relative text-center mt-20 mb-8 p-12 rounded-lg bg-black bg-opacity-20'>
+            <div className='absolute left-0 right-0 -top-10'>
+                <Image
+                    className='align-middle rounded-full'
+                    unoptimized
+                    height='70px'
+                    width='70px'
+                    src={author.photo.url}
+                    alt={author.name}
+                />
+            </div>
+            <h3 className='text-white my-4 font-bold text-xl'>{author.name}</h3>
+            <p className='text-white text-lg'>{author.bio}</p>
         </div>
     )
 }
